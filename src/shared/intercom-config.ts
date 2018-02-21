de732ceb1c7b61e14c4207607e92d25d7c8db4f5
Dustin Blanchard <dustinblanchard@users.noreply.github.com>
2018-02-20 18:12:43 -0600

    Update intercom-config.ts
    Add alignment and padding options

--- src/shared/intercom-config.ts
+++ src/shared/intercom-config.ts
@@ -1,4 +1,18 @@
 export class IntercomConfig {
     appId: string;
     updateOnRouterChange?: boolean;
+     /**
+     * Customize left or right position of messenger
+     */
+    alignment?: 'left' | 'right';
+
+    /**
+     * Customize horizontal padding
+     */
+    horizontal_padding?: number;
+
+    /**
+     * Customize vertical padding
+     */
+    vertical_padding?: number;
 }
